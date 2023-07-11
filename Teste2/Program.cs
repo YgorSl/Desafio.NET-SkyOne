cefe14794776f4ef8a9900e73f6f50bccb904994
Paulo Sergio Lourenço <ygorguito16@gmail.com>
2023-07-11 19:24:13 -0300

    Teste 2 Ok

---
+++ Teste2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Bem vindo ao Teste 02, para iniciarmos qual é o seu Nome?");
+string nome = Console.ReadLine();
+double qtdNota = 0;
+double nota = 0;
+double total = 0;
+calcularNota();
+
+void calcularNota()
+{   
+    Console.WriteLine($"{nome} por favor nos informe qual foi sua nota na prova");
+    nota = Convert.ToDouble(Console.ReadLine());
+    qtdNota++;
+    total += nota;
+    double media  = total / qtdNota;
+    Console.WriteLine($"Nome = {nome}\nQuantidade de Provas: {qtdNota}\nTotal de Nota{total}\nsua média é de {media}\n");
+
+    calcularNota();
+}
