0bce924fb2433dcb888e0642ed7d0c1143100371
Paulo Sergio Lourenço <ygorguito16@gmail.com>
2023-07-11 19:01:47 -0300

    Teste 1OK

--- Desafio.NET/Program.cs
+++ Desafio.NET/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Digite a nota do primeiro semestre");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a nota do segundo semestre");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+double media = (nota1 + nota2) / 2;
+Console.WriteLine($"Sua média é de {media}");
