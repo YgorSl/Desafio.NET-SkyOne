6f66b07eff8415c9ee05f3a56a669a96b62efb8e
Paulo Sergio Lourenço <ygorguito16@gmail.com>
2023-07-11 21:11:25 -0300

    Teste 3 Ok

---
+++ Teste 3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Bem Vindo ao Teste 03");
+calcularMedia();
+
+void calcularMedia()
+{
+    Console.WriteLine("Informe o nome do Aluno:");
+
+    string nome = Console.ReadLine();
+    Console.WriteLine($"Qual foi a nota do primeiro semestre do aluno {nome}");
+    double nota1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"Qual foi a nota do segundo semestre do aluno {nome}");
+    double nota2 = Convert.ToDouble(Console.ReadLine());
+
+    double media = (nota1 + nota2) / 2;
+    Console.WriteLine($"Nome do aluno: {nome}");
+    Console.WriteLine($"Média = {media}");
+
+    if (media > 6)
+    {
+        Console.WriteLine("O Aluno foi: Aprovado\n");
+    }
+    else Console.WriteLine("O aluno foi Reprovado\n");
+    calcularMedia();
+}
